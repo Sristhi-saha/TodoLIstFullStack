c05fefd62df7e2d18397ad83f7201620752b16c3
Sristhi-saha <shyamalsaha558@gmail.com>
2026-01-09 07:43:51 +0530

    add controllers

---
+++ server/controllers/todolist_controller.js
@@ -0,0 +1,71 @@
+const todo = require('../models/todolist.js');
+
+const getAllTodos = async(req,res)=>{
+    try{
+        const allTodos = await todo.find({});
+        res.json(allTodos);
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const addTodos = async(req,res)=>{
+    try{
+        const todoData = req.body;
+        const newTodos = new todo(todoData);
+        await newTodos.save();
+        res.status(201).json({
+            success:true,
+            data:newTodos
+        })
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const deleteTodos = async(req,res)=>{
+    try{
+        const todoId = req.params.id;
+        const deletedTodo = await todo.findByIdAndDelete(todoId);
+        if(!deletedTodo){
+            return res.status(404).json({
+                success:false,
+                message:"Todo not found with this id"
+            });
+        }else{
+            res.status(200).json({
+                success:true,
+                message:"Todo deleted successfully"
+            })
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const updateTodos = async(req,res)=>{
+    try{
+        const todoId = req.params.id;
+        const todoData = req.body;
+        const updateTodo = await todo.findByIdAndUpdate(todoId, todoData, { new: true });
+        res.status(200).json({
+            success:true,
+            data:updateTodo
+        })
+        if(!updateTodo){
+            return res.status(404).json({
+                success:false,
+                message:"Todo not found with this id"
+            });
+        }
+    }catch(e){
+        console.log(e)
+    }
+}
+
+module.exports = {
+    getAllTodos,
+    addTodos,
+    deleteTodos,
+    updateTodos
+}
